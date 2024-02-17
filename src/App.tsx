3501cd72e8608004ab6e2e84f2605d85821d8f9d
VladKlimakhovich <klimaxovich@mail.ru>
2024-02-17 22:20:28 +0400

    add project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+
+
 import './App.css';
 
 function App() {
+  //let count = 0;
+  const [count, setCount] = React.useState(0)
+  const onClickPlus = () =>{
+    setCount(count + 1);
+  }
+
+  const onClickMinus = () =>{
+    setCount(count - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <h2>Counter:</h2>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">- Minus</button>
+        <button onClick={onClickPlus} className="plus">Plus +</button>
+      </div>
     </div>
   );
 }
